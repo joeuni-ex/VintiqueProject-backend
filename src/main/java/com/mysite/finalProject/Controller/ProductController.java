31fe677b32fdbcee423e92a0bc6c22f5572ce3dc
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-09 14:31:31 +0900

    제품 컨트롤러

--- src/main/java/com/mysite/finalProject/Controller/ProductController.java
+++ src/main/java/com/mysite/finalProject/Controller/ProductController.java
@@ -1,19 +1,37 @@
 package com.mysite.finalProject.Controller;
 
+import com.mysite.finalProject.Service.ProductService;
+import com.mysite.finalProject.model.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/product")
+@RequestMapping("api/product")
 @RestController
-
+@RequiredArgsConstructor
 public class ProductController {
 
-    @GetMapping("/list")
-    public String list(String text, Model model){
+    private final ProductService productService;
 
-        return "/list";
+    //제품 추가하기
+    @PostMapping
+    public ResponseEntity<Object> saveProduct(@RequestBody Product product){
+        return new ResponseEntity<>(productService.saveProduct(product), HttpStatus.CREATED);
+    }
+
+    //전체 제품 조회하기
+    @GetMapping
+    public ResponseEntity<Object> getAllProducts(){
+        return new ResponseEntity<>(productService.findeAllProducts(), HttpStatus.OK);
+    }
+
+    //제품 삭제하기
+    @DeleteMapping("{productId}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable  Long productId){
+        productService.deleteProduct(productId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
