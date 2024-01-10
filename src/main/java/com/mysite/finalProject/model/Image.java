2d7e0f3086bbe6a8cb2d0d84433464f808859dda
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-10 09:05:24 +0900

    이미지 모델 수정-> db 추가 가능

--- src/main/java/com/mysite/finalProject/model/Image.java
+++ src/main/java/com/mysite/finalProject/model/Image.java
@@ -17,10 +17,6 @@ public class Image {
     @Column(name = "product_id", nullable = false)
     private Long productId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", referencedColumnName = "id", insertable = false, updatable = false)
-    private Product product;
-
     @Column(name = "image" ,nullable = false)
     private String image;
 
