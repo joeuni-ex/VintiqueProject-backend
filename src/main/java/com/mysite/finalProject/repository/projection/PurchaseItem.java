66510a1f30d943f03fdfbae03736d6a68cca72c7
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-17 10:31:54 +0900

    purchaseItem 데이터 추가

--- src/main/java/com/mysite/finalProject/repository/projection/PurchaseItem.java
+++ src/main/java/com/mysite/finalProject/repository/projection/PurchaseItem.java
@@ -5,8 +5,12 @@ import java.time.LocalDateTime;
 public interface PurchaseItem {
     //프로젝션 (Projection) 프로젝션이란 SELECT 절에서 조회할 대상을 지정하는 것
     //여기서 제품의 이름,가격,구매시간 만을 가져오는 프로젝션을 만들었다.
+
+    Long getUserId();
+    Long getProductId();
     String getName();
     Integer getPrice();
+    String getMainImage();
     LocalDateTime getPurchaseTime();
 
 }
